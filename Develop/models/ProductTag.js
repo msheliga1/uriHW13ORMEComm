fbd6fe853c332756b341548e96c9f09e72073d06
Mike Sheliga <msheliga1@gmail.com>
2024-02-26 12:33:03 -0500

    Added product-tag model, initializes goot!

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -4,9 +4,37 @@ const sequelize = require('../config/connection');
 
 class ProductTag extends Model {}
 
+// ProductTag
+//     id            Integer    NOT null values    primary key    auto increment
+//     product_id    Integer    References the product model's id
+//     tag_id        Integer    References the tag model's id
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // This column will store a reference of the `id` of the `Product` that owns this Product-tag
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // This references the `product` model, which we set in `Product.js`
+        model: 'product',
+        key: 'id',
+      },
+    },
+    // This column will store a reference of the `id` of the `Tag` that owns this Product-tag
+    tag_id: {
+          type: DataTypes.INTEGER,
+          references: {
+            // This references the `tag` model, which we set in `Tag.js`
+            model: 'tag',
+            key: 'id',
+          },
+    },
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,36 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products belongsTo Category - The association can also be created from the Procuct side
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.hasOne(Tag, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
+
+// Driver.hasOne(License, {
+//  foreignKey: 'driver_id',
+//  onDelete: 'CASCADE',
+// });
+
+// License.belongsTo(Driver, {
+//   foreignKey: 'driver_id',
+// });
 
 module.exports = {
   Product,
